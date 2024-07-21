aec99f62bed86452607be4aa8ae9a7707149a8a4
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-07-21 18:40:54 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,20 +1,3 @@
 function solution(s) {
-    // 단어별로 나누기
-    let words = s.split(' ');
-
-    // 각 단어별로 순회
-    let transformedWords = words.map(word => {
-        // 단어의 각 문자를 순회하면서 반환
-        let transformedWord = word.split('').map((char,index) => {
-            if( index%2 === 0 ){
-                return char.toUpperCase();
-            }
-            else {
-                return char.toLowerCase();
-            }
-        }).join('');
-        return transformedWord;
-    })
-
-    return transformedWords.join(' ');
+    return s.toUpperCase().replace(/(\w)(\w)/g, function(a){return a[0].toUpperCase()+a[1].toLowerCase();})
 }
