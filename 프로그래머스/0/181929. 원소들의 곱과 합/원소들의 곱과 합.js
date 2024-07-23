b0e584f388e7be6c069faa9ea49a2e124d200fed
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-07-23 23:13:25 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    let sum = 0;
+    let multiply = 1;
+    
+    for ( let i = 0; i < num_list.length ; i++){
+        sum+= num_list[i];
+        multiply *= num_list[i];
+    }
+    
+    if( multiply < Math.pow(sum,2)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
