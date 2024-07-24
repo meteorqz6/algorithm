80a31f00c5c66679ab80c911910216b1fbfe3009
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-07-24 21:13:00 +0900

    [level 0] Title: 수 조작하기 1, Time: 23.04 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,19 @@
+function solution(n, control) {
+    let arr = control.split('');
+    
+    for( let i = 0 ; i < arr.length; i++){
+        if(arr[i] === 'w'){
+            n += 1;
+        }
+        else if(arr[i] === 'a'){
+            n -= 10;
+        }
+        else if(arr[i] === 's'){
+            n -= 1;
+        }
+        else{
+            n += 10;
+        }
+    }
+    return n;
+}
