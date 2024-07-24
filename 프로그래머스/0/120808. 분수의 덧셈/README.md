29d9898bdf1443ef361a6a8db1a77e7bba75a6c3
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-07-25 00:40:57 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.10 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 22:11:21
+2024년 07월 25일 00:40:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,28 +1,11 @@
-function gcd(a, b) {
-    while ( b!== 0 ) {
-        let tmp = b;
-        b = a % b;
-        a = tmp;
-    }
-    return a;
-}
-
-function lcm(a, b) {
-    return a*b / gcd(a, b);
+function fnGCD(a, b){
+    return (a%b) ? fnGCD(b,a%b) : b;
 }
 
 function solution(numer1, denom1, numer2, denom2) {
+    let num = numer1*denom2 + numer2*denom1;
+    let denom = denom1*denom2;
+    let gcd = fnGCD(num, denom);
     
-    let n1 = lcm(denom1, denom2);
-    let n2 = n1/denom1*numer1 + n1/denom2*numer2;
-    
-    if(gcd(n1,n2) !== 1){
-        return [n2/gcd(n1,n2) , n1/gcd(n1,n2)]
-    }
-    else{
-        return [n2,n1];
-    }
-    
-   // return [(n2/denom1*numer1 + n2/denom2*numer2), n2];
+    return [num/gcd, denom/gcd];
 }
-
