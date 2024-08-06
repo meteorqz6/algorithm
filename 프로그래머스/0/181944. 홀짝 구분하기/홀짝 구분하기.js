ea3d69c1c3eb2c142a24ecfd80fc526cb5a6cce7
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-08-06 20:11:55 +0900

    [level 0] Title: 홀짝 구분하기, Time: 61.86 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -10,9 +10,9 @@ rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
     n = Number(input[0]);
-    if(n%2 === 0){
-        console.log(n+' is even');
-    }else{
-        console.log(n+' is odd');
+    if(n%2 === 0) {
+        console.log(`${n} is even`);
+    } else {
+        console.log(`${n} is odd`);
     }
 });
