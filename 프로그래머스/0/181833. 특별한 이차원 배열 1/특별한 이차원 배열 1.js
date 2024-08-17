07df04785366001940284c5bbecd1dc34c8fe96c
Yooseong <102887277+meteorqz6@users.noreply.github.com>
2024-08-17 20:41:10 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,10 @@
+function solution(n) {
+   
+    let arr = new Array(n).fill().map(() => new Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        arr[i][i] = 1;
+    }
+
+    return arr;
+}
