d9f6247bdb934f669e2170ac092e59fe3162d692
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2025-04-11 21:52:39 +0900

    [Bronze III] Title: 세 수, Time: 108 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+input.sort((a, b) => a - b);
+console.log(input[1]);
