f45fca7f23a989385774fcb7f4cc2d04df2c2daa
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2025-07-21 15:55:04 +0900

    [Silver II] Title: 섬의 개수, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -2,37 +2,41 @@ from sys import stdin
 from collections import deque
 input = stdin.readline 
 
-dx = [1, -1, 0, 0, 1, -1, 1, -1] # 그래프의 탐색 방향
+# 총 8방향
+dx = [1, -1, 0, 0, 1, -1, 1, -1]
 dy = [0, 0, 1, -1, 1, -1, -1, 1]
 
+# BFS 함수 정의
 def bfs(x, y):
     que = deque()
     que.append((x, y))
-    graph[y][x] = 0
+    graph[y][x] = 0  # 시작 지점 방문 처리
     while que:
-       a, b = que.pop()
-       for i in range(8):
-           ny = b + dy[i]
-           nx = a + dx[i]
-           if 0<=nx<m and 0<=ny<n and graph[ny][nx] == 1: # 그래프의 탐색조건
-               que.append((nx, ny))
-               graph[ny][nx] = 0
-
+        a, b = que.popleft()
+        for i in range(8):  # 8방향 탐색
+            ny = b + dy[i]
+            nx = a + dx[i]
+            # 범위 내에 있고, 아직 방문 안 한 땅인 경우
+            if 0 <= nx < m and 0 <= ny < n and graph[ny][nx] == 1:
+                que.append((nx, ny)) # 다음에 방문할 곳으로 추가
+                graph[ny][nx] = 0  # 방문 처리
 
 result = []
+
 while True:
-    res = 0
-    m, n = map(int, input().split())
+    res = 0 # 지도에서 찾은 섬의 개수
+    m, n = map(int, input().split())  # m: 너비(열), n: 높이(행)
     if n == 0 and m == 0:
-        break
+        break # 입력 종료 조건
     graph = [list(map(int, input().split())) for _ in range(n)]
 
     for i in range(n):
         for j in range(m):
-            if graph[i][j] == 1: # 해당지점이 땅인 경우만 탐색
-                bfs(j, i)
-                res += 1 # 땅인경우 결과값 +1
-    result.append(res) # 결과 리스트에 추가
+            if graph[i][j] == 1: # 아직 방문하지 않은 땅이라면
+                bfs(j, i) # 연결된 땅들을 모두 찾아서 0으로 만들기
+                res += 1  # 새로운 섬 발견
+    result.append(res)
 
+# 결과 출력
 for i in result:
-    print(i) # 결과 출력
+    print(i)
