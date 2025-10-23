bc352a26b6483234eec9c6f5f4a8a1ed0175e461
Nam Yooseong <102887277+meteorqz6@users.noreply.github.com>
2025-10-23 18:10:01 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.57 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,14 +1,13 @@
 function solution(numbers) {
-    let answer = [];
-    for ( let i = 0; i < numbers.length ; i++ ){
-        for ( let j = i+1 ; j < numbers.length ; j++){
-            if(answer.indexOf(numbers[i]+numbers[j]) === -1 )
-            answer.push(numbers[i]+numbers[j]);
+    const n = numbers.length;
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            arr.push(numbers[i] + numbers[j]);
         }
     }
-    return answer.sort((a,b)=>a-b);
+    
+    const result = [...new Set(arr)];
+    result.sort((a, b) => a - b);
+    return result;
 }
-// 0 1234
-// 1 234
-// 2 34
-// 3 4   11234
